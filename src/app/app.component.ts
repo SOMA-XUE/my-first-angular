ae9db105bfdbc6727f685df5ec62eb1ac646f254
xue wei <v.xuewei@shengqugames.com>
2021-03-03 18:11:45 +0800

    xx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2021-03-01 16:02:12
- * @LastEditTime: 2021-03-01 18:10:08
+ * @LastEditTime: 2021-03-02 17:06:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \my-first-angular\src\app\app.component.ts
  */
 import { Component } from '@angular/core';
 
+// interface {
+//   Item: object;
+// }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,4 +20,32 @@ export class AppComponent {
   title = 'my-first-angular-project';
   title2 = 'my-first-angular-project';
   name = 'your';
+  imgSrc = 'https://paystatic.sdoprofile.com/img/logo@1x.png';
+  btn1Status = true;
+  ngif1 = true;
+  noServer = true;
+  items = [
+    {
+      name: '1111'
+    },
+    {
+      name: '222'
+    }
+  ]
+
+  btn2Handler() {
+    this.ngif1 = !this.ngif1;
+    this.name = 'my my'
+  }
+
+  inputHandler(event:any) {
+    console.log((<HTMLInputElement>event.target).value)
+    this.name = (<HTMLInputElement>event.target).value
+
+    
+  }
+
+  trackByItems(index: number, item: any): number {
+    return item.id!;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
